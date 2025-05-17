489d670783d8fec855a5d3a03e8658bc6dc510c8
r3lllax <turkevicaleksandr@gmail.com>
2025-05-17 23:51:44 +0300

    Update AddUserToWS.vue

--- src/components/AddUserToWS.vue
+++ src/components/AddUserToWS.vue
@@ -43,7 +43,7 @@ const AddToWs = async (WSid)=>{
 </script>
 
 <template>
-  <form action="" class="border w-full h-full p-5 flex flex-col items-center gap-2.5">
+  <form action="" class="border w-full h-fit p-5 flex flex-col items-center gap-2.5">
     <div class="w-full">
       <h2 class="text-nowrap text-xl font-bold dark:text-white w-fit">Смены пользователя</h2>
       <ul class="border rounded-xl grid grid-cols-12 overflow-auto gap-2 p-5 justify-center" style="max-height: 15rem;" v-if="WorkShiftInfo.Work_shifts.length>0">
@@ -68,7 +68,7 @@ const AddToWs = async (WSid)=>{
             <span class="font-bold font-sans">Смена {{no_work_shift.id}}</span>
             <span class="text-sm flex flex-col text-center text-black">Дата начала: <span class=" text-black font-bold p-1 bg-sky-200 rounded-full md:rounded">{{no_work_shift.start}}</span></span>
             <span class="text-sm flex flex-col text-center text-black">Дата окончания: <span class=" text-black font-bold p-1 bg-sky-200 rounded-full md:rounded">{{no_work_shift.end}}</span></span>
-            <button @click.prevent="AddToWs(no_work_shift.id)" class="mt-5 cursor-pointer inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-fit">Добавить</button>
+            <button @click.prevent="AddToWs(no_work_shift.id)" class="mt-5 cursor-pointer w-full items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-fit">Добавить</button>
 
           </div>
         </li>
