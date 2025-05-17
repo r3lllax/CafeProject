aeb1f441daa67fcb729226558b1f85aae1c9c30c
r3lllax <turkevicaleksandr@gmail.com>
2025-05-17 23:34:39 +0300

    Логин блюр и категории пользователей

--- src/views/Admin/UsersView.vue
+++ src/views/Admin/UsersView.vue
@@ -5,14 +5,41 @@ import apiFetch from '@/helpers/apiFetch.js'
 import CreateUserForm from '@/components/CreateUserForm.vue'
 import AddUserToWS from '@/components/AddUserToWS.vue'
 
-const data = ref([])
+const users = ref({
+  Admins:{
+    prefix:'',
+    users:[],
+  },
+  Waiters:{
+    prefix:'',
+    users:[],
+  },
+  Cooks:{
+    prefix:'',
+    users:[],
+  },
+})
 const modalAdd =ref(false)
 const modalWs =ref(false)
 onMounted(async ()=>{
   const responce = await apiFetch('GET','/user')
   for(let user of responce.users){
-    data.value.push(user)
+    switch (user.group){
+      case "Администратор":
+        users.value.Admins.users.push(user)
+        users.value.Admins.prefix = user.group + "ы"
+        break
+      case "Официант":
+        users.value.Waiters.users.push(user)
+        users.value.Waiters.prefix = user.group + "ы"
+        break
+      case "Повар":
+        users.value.Cooks.users.push(user)
+        users.value.Cooks.prefix = user.group + "а"
+        break
+    }
   }
+  console.log(users)
 })
 
 const ViewUserID = ref(null)
@@ -44,15 +71,15 @@ const CloseModal = () =>{
 </script>
 
 <template>
-  {{"ДОБАВИТЬ КАТЕГОРИИ АДМИН ПОВАР ОФИЦИАНТ"}}
   <main class="w-full flex justify-center">
     <div class="w-full md:w-1/2 self-center justify-center">
       <div class="p-5">
         <h2 class="text-4xl font-bold dark:text-white">Пользователи</h2>
       </div>
-      <div class="w-full">
+      <div class="w-full mb-5" v-for="item of users">
+        <h2 class="text-xl mb-2 font-bold dark:text-white">{{item.prefix}}</h2>
           <ul class="grid grid-cols-12 gap-4">
-            <li class="w-full col-span-full md:col-span-6 lg:col-span-4" v-for="user of data">
+            <li class="w-full col-span-full md:col-span-6 lg:col-span-4" v-for="user of item.users">
               <div class="w-full bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex justify-end px-4 pt-4">
                   <span class="text-sm text-gray-500 dark:text-gray-400">{{ user.id }}</span>
@@ -62,6 +89,7 @@ const CloseModal = () =>{
                   <img class=" object-cover w-24 h-24 mb-3 rounded-full shadow-lg" :src="`https://apicafe/public/${user.photo_file?user.photo_file:'photos/NoUserPhoto.jpg'}`" alt="Bonnie image"/>
                   <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{user.name}}</h5>
                   <span class="text-sm text-gray-500 dark:text-gray-400">{{ user.group }}</span>
+                  <span class="transition-all cursor-default mt-3 text-sm text-gray-100 dark:text-gray-400 bg-gray-400 py-1 px-4 rounded-full blur-sm hover:blur-none">{{ user.login }}</span>
                   <div class="flex mt-4 md:mt-6">
                     <a @click.prevent="ShowModalAndID(user.id)" :class="{'bg-blue-300':user.group=='Администратор','bg-blue-700':user.group!=='Администратор','pointer-events-none': user.group=='Администратор'}" class="z-19 cursor-pointer inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Назначить на смену</a>
                   </div>
