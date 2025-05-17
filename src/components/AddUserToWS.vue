b9c4e7d104a7af9b77bb4822b7160968d3898b38
r3lllax <turkevicaleksandr@gmail.com>
2025-05-18 01:17:23 +0300

    final day edit

--- src/components/AddUserToWS.vue
+++ src/components/AddUserToWS.vue
@@ -59,7 +59,7 @@ const AddToWs = async (WSid)=>{
           </div>
         </li>
       </ul>
-      <div class="flex flex-nowrap" v-else-if="WorkShiftInfo.Processing">
+      <div class="flex flex-nowrap bg-sky-300 py-1 px-4 rounded-xl items-center text-white justify-center gap-1" v-else-if="WorkShiftInfo.Processing">
         <span>Загрузка...</span>
         <svg aria-hidden="true" class="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
@@ -84,7 +84,7 @@ const AddToWs = async (WSid)=>{
           </div>
         </li>
       </ul>
-      <div class="flex flex-nowrap" v-else-if="WorkShiftInfo.Processing">
+      <div class="flex flex-nowrap bg-sky-300 py-1 px-4 rounded-xl items-center text-white justify-center gap-1" v-else-if="WorkShiftInfo.Processing">
         <span>Загрузка...</span>
         <svg aria-hidden="true" class="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
