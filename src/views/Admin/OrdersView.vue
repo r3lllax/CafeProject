fe0236d695ad717dd0995ae7c4cb41e710d1967f
r3lllax <turkevicaleksandr@gmail.com>
2025-05-18 18:22:53 +0300

    Добавление смены и спиннеры

--- src/views/Admin/OrdersView.vue
+++ src/views/Admin/OrdersView.vue
@@ -3,8 +3,19 @@
 
 <template>
   <main class="w-full flex justify-center">
-    <div class="w-full md:w-1/2 self-center border justify-center">
-      Менять на анимированно позишн абсолют при расширенном просмотре заказа
+    <div class="px-2 md:px-0 w-full md:w-1/2 self-center justify-center flex flex-col items-center">
+      <div class="w-full flex flex-col md:flex-row my-5 gap-2">
+        <input type="text" id="end" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light" placeholder="ID смены"  />
+        <button @click.prevent="toggleModal" class="cursor-pointer w-full md:w-fit items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Назад</button>
+      </div>
+      <div class="bg-gray-100 w-full h-screen rounded-xl">
+        <tempalate v-if="0">
+          <div class="w-full h-full flex justify-center items-center">
+            <span class="text-4xl font-semibold text-neutral-700">Тут пусто</span>
+          </div>
+        </tempalate>
+      </div>
+
     </div>
   </main>
 </template>
--- src/views/Admin/WorkShiftsView.vue
+++ src/views/Admin/WorkShiftsView.vue
@@ -7,16 +7,18 @@ import ErrorDescription from '@/components/ErrorDescription.vue'
 
 const data = ref({
   WorkShifts:[],
-  Errors:{}
+  Errors:{},
+  WorkShiftWasLoad:false
 })
 
 const LoadWorkShifts = async () =>{
+  data.value.WorkShiftWasLoad = false
   const responce = await apiFetch("get","/work-shift")
   data.value.WorkShifts = responce
   for(let ws of data.value.WorkShifts){
     checkboxes.value.push( {id:ws.id,checked:ws.active?true:false,processing:false})
   }
-
+  data.value.WorkShiftWasLoad = true
 
 }
 onMounted(()=>{
@@ -50,12 +52,53 @@ const check = async (WorkShift)=>{
   checkbox.processing = false
 
 }
+
+const NewWsForm = ref({
+  data:{
+    start:'',
+    end:'',
+  },
+  errors:{},
+  processing:false,
+})
+
+const modal = ref({
+  OpenStatus:false
+})
+
+const toggleModal = () =>{
+  modal.value.OpenStatus = !modal.value.OpenStatus
+}
+
+
+const MakeWS = async ()=>{
+  NewWsForm.value.errors = []
+  if(NewWsForm.value.processing){
+    return
+  }
+  NewWsForm.value.processing =true
+  const responce = await apiFetch("POST","/work-shift",NewWsForm.value.data)
+  console.log(responce)
+  if(responce.error){
+    if(!responce.error.errors){
+      NewWsForm.value.errors.start=[responce.error.message]
+    }
+    else{
+      NewWsForm.value.errors=responce.error.errors
+    }
+  }
+  if(responce.data){
+    LoadWorkShifts()
+    toggleModal()
+  }
+  NewWsForm.value.processing =false
+}
 </script>
 
 <template>
-  <main class="w-full flex justify-center">
-    <div class="w-full md:w-1/2 self-center border justify-center gap-3">
-      <h2 class="text-4xl">Рабочие смены</h2>
+  <main class="w-full flex flex-col items-center">
+    <h2 class="text-4xl font-bold dark:text-white p-5">Рабочие смены</h2>
+    <div v-if="data.WorkShiftWasLoad" class="w-full md:w-1/2 self-center justify-center gap-3">
       <div class="w-full">
         <ul class="grid grid-cols-12 gap-3 p-2">
           <li class="col-span-full">
@@ -88,8 +131,40 @@ const check = async (WorkShift)=>{
             </label>
 
           </li>
-         </ul>
-      </div>
+
+          <li @click.prevent="toggleModal" class="hover:bg-blue-400 hover:text-white hover:scale-[1.01] relative transition cursor-pointer border-gray-200 border col-span-12 p-6 bg-white rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center text-neutral-500">
+            <div class="w-full h-full peer absolute top-0 bottom-0 right-0 left-0"></div>
+            <span class="transition duration-700 peer-hover:scale-[2] peer-hover:rotate-[360deg] peer-hover:font-bold">+</span>
+          </li>
+        </ul>
+      </div >
+
+    </div>
+    <div v-else class="w-full flex justify-center">
+      <svg aria-hidden="true" class="w-7 h-7 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+      </svg>
     </div>
   </main>
+  <div :class="{'hidden':!modal.OpenStatus}" class="fixed bg-black/50 top-0 bottom-0 left-0 right-0 z-50 flex justify-center items-center">
+    <div class="w-full md:w-1/2 max-h-full flex justify-center flex-col items-center bg-white gap-2 overflow-y-auto p-5 rounded-none md:rounded-xl">
+      <h2 class="text-4xl mb-10">Добавление смены</h2>
+      <div class="w-full flex flex-col gap-2">
+        <h3 class="text-2xl">Дата и время начала</h3>
+        <input v-model="NewWsForm.data.start" :class="{'bg-red-200':NewWsForm.errors.start,'bg-gray-50':!NewWsForm.errors.start}" type="text" id="start" class="shadow-xs border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light" placeholder="2000-01-01 08:00"  />
+        <ErrorDescription :error="NewWsForm.errors.start"></ErrorDescription>
+      </div>
+      <div class="w-full flex flex-col gap-2">
+        <h3 class="text-2xl">Дата и время окончания</h3>
+        <input v-model="NewWsForm.data.end" :class="{'bg-red-200':NewWsForm.errors.end,'bg-gray-50':!NewWsForm.errors.end}" type="text" id="end" class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light" placeholder="2000-01-01 08:00"  />
+        <ErrorDescription :error="NewWsForm.errors.end"></ErrorDescription>
+      </div>
+
+
+      <button @click.prevent="MakeWS" class="mt-5 cursor-pointer w-full items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-fit">Создать</button>
+      <button @click.prevent="toggleModal" class="cursor-pointer w-full items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-fit">Назад</button>
+
+    </div>
+  </div>
 </template>
