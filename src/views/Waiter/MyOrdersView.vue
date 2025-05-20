40a25be66a70ef6a310a8f5c7e848524043bf596
r3lllax <turkevicaleksandr@gmail.com>
2025-05-20 19:49:15 +0300

    waiter orders visual

--- src/views/Waiter/MyOrdersView.vue
+++ src/views/Waiter/MyOrdersView.vue
@@ -26,37 +26,77 @@ onMounted( async ()=>{
   MyOrders.value.isProcessing = false
   console.log(MyOrders.value.data.length)
 })
+
+const orderFullScreen = ref({
+  state:false,
+  id:null,
+})
+
+const OrderToFullScreenOrBack = (id)=>{
+  orderFullScreen.value.id = null
+  console.log(orderFullScreen.value)
+  let order = document.getElementById(`${id}`)
+  if(orderFullScreen.value.state){
+    order.classList.remove('fixed','z-50','bg-white','top-0','bottom-0','left-0','right-0','backdrop-blur-xl')
+    order.classList.add('hover:scale-110','bg-white','rounded-xl')
+
+  }
+  else{
+    order.classList.add('fixed','z-50','bg-white','top-0','bottom-0','left-0','right-0','backdrop-blur-xl')
+    order.classList.remove('hover:scale-110','bg-white','rounded-xl')
+    orderFullScreen.value.id = id
+  }
+  orderFullScreen.value.state = !orderFullScreen.value.state
+
+
+}
+
 </script>
 
 <template>
   <main class="w-full flex justify-center">
-    <div class="w-full md:w-1/2 self-center justify-center p-5 flex gap-3 flex-col">
+    <div class=" w-full md:w-1/2 self-center justify-center p-5 flex gap-3 flex-col">
       <h2 class="text-4xl font-bold">Ваши заказы:</h2>
       <li @click.prevent="toggleModal" class="hover:bg-blue-400 hover:text-white hover:scale-[1.01] relative transition cursor-pointer border-gray-200 border col-span-12 p-6 bg-white rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center text-neutral-500">
         <div class="w-full h-full peer absolute top-0 bottom-0 right-0 left-0"></div>
         <span class="transition duration-700 peer-hover:scale-[2] peer-hover:rotate-[360deg] peer-hover:font-bold">+</span>
       </li>
-      <ul class="grid grid-cols-12 gap-3" v-if="MyOrders.data.length">
-        <li class="transition items-center hover:scale-110 shadow-lg border col-span-12 sm:col-span-6 xl:col-span-4 min-h-28 min-w-28 rounded-xl flex flex-col gap-3 justify-between p-5" v-for="order of MyOrders.data">
-          <div class="w-fit md:w-full flex flex-col items-center justify-center">
-            <div class="w-full md:justify-between flex flex-col md:flex-row items-center">
-              <div :class="{'bg-red-500 ':order.status=='Отменен','bg-sky-500':order.status=='Принят','bg-yellow-500':order.status=='Готовится','bg-green-600 ':order.status=='Готов','bg-green-400 ':order.status=='Оплачен',}" class="px-3 py-[0.5rem] m-0 border border-neutral-400 rounded-full font-bold text-xl text-white">
-                {{ order.id }}
+      <div class="w-full h-full" v-if="MyOrders.data.length">
+        <ul class="grid grid-cols-12 gap-3">
+          <li :id="order.id" class="overflow-y-auto transition items-center hover:scale-110 shadow-lg border col-span-12 sm:col-span-6 xl:col-span-4 rounded-xl flex flex-col gap-3 justify-between p-5" v-for="order of MyOrders.data">
+            <div :class="{'justify-center':!orderFullScreen.state,'h-full':orderFullScreen.state}" class="w-fit md:w-full flex flex-col items-center">
+              <div class="w-full md:justify-between flex flex-col md:flex-row items-center">
+                <div :class="{'bg-red-500 ':order.status=='Отменен','bg-sky-500':order.status=='Принят','bg-yellow-500':order.status=='Готовится','bg-green-600 ':order.status=='Готов','bg-green-400 ':order.status=='Оплачен',}" class="px-3 py-[0.5rem] m-0 border border-neutral-400 rounded-full font-bold text-xl text-white">
+                  {{ order.id }}
+                </div>
+                <div class="p-1 flex gap-1 items-center items-center flex-col">
+                  <span class="text-lg font-semibold">Статус:</span>
+                  <span :class="{'bg-red-500 ':order.status=='Отменен','bg-sky-500':order.status=='Принят','bg-yellow-500':order.status=='Готовится','bg-green-600 ':order.status=='Готов','bg-green-400 ':order.status=='Оплачен',}" class="px-1 text-white font-semibold rounded-full">{{ order.status }}</span>
+                </div>
               </div>
-              <div class="p-1 flex gap-1 items-center items-center flex-col">
-                <span class="text-lg font-semibold">Статус:</span>
-                <span :class="{'bg-red-500 ':order.status=='Отменен','bg-sky-500':order.status=='Принят','bg-yellow-500':order.status=='Готовится','bg-green-600 ':order.status=='Готов','bg-green-400 ':order.status=='Оплачен',}" class="px-1 text-white font-semibold rounded-full">{{ order.status }}</span>
+              <div class="w-full flex flex-col gap-3 py-2" v-if="orderFullScreen.state && orderFullScreen.id==order.id">
+                <div class="text-center text-4xl p-2 text-white rounded-xl bg-sky-500">Позиции(ID,Название,Количество,Цена):</div>
+                <ul class="flex flex-col gap-3 w-full">
+                  <li class="p-5 rounded-xl shadow-xl w-full flex flex-col md:flex-row text-center justify-between items-center bg-white" v-for="position of order.positions">
+                    <div class="font-semibold border-b  md:border-b-0 w-full p-2 text-xl">{{position.id}}</div>
+                    <div class="font-semibold border-b  md:border-b-0 w-full p-2 text-xl overflow-hidden text-ellipsis">{{position.position}}</div>
+                    <div class="font-semibold border-b  md:border-b-0 w-full p-2 text-xl">{{position.count}}</div>
+                    <div class="font-semibold border-b  md:border-b-0 w-full p-2 text-xl">{{position.price}}</div>
+                  </li>
+                </ul>
+              </div>
+              <div class="w-full flex justify-center items-center">
+                <button :id="'Button'+order.id" @click.prevent="OrderToFullScreenOrBack(order.id)" class="z-1 transition ease-out overflow-hidden relative hover:scale-110 border w-fit px-3 py-2 rounded-2xl flex items-center text-white before:transition before:ease-out before:duration-700 before:absolute before:border before:bg-sky-800 before:rounded-full before:min-w-10 before:min-h-10 before:text-white before:translate-x-[6rem] before:translate-y-[2.6rem] hover:before:scale-[10] hover:text-white hover:shadow-lg before:-z-10 font-semibold bg-sky-500">
+                  <template v-if="orderFullScreen.state">Закрыть</template>
+                  <template v-else>Подробнее</template>
+                </button>
               </div>
             </div>
-            <div class="w-full flex justify-center items-center">
-              <button class="z-1 transition ease-out overflow-hidden relative hover:scale-110 border w-fit px-3 py-2 rounded-2xl flex items-center text-white before:transition before:ease-out before:duration-700 before:absolute before:border before:bg-sky-800 before:rounded-full before:min-w-10 before:min-h-10 before:text-white before:translate-x-[6rem] before:translate-y-[2.6rem] hover:before:scale-[10] hover:text-white hover:shadow-lg before:-z-10 font-semibold bg-sky-500">
-                Подробнее
-              </button>
-            </div>
-          </div>
-        </li>
+          </li>
 
-      </ul>
+        </ul>
+      </div>
+
       <div class="shadow-lg p-10 rounded-xl text-center flex flex-col justify-center items-center" v-else-if="MyOrders.errors.error">
         <span class="text-4xl font-semibold">
                  {{MyOrders.errors.error}}
